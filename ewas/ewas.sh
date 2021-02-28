f640741598da5454168f4ab0895a9bb320226598
Joseph Wakim <jwakim@stanford.edu>
2021-02-28 01:57:17 -0800

    fixed typo

--- ewas/ewas.sh
+++ ewas/ewas.sh
@@ -8,5 +8,5 @@ python ewas_prep.py
 echo "Running EWAS..."
 cd GLINT_1
 python glint.py --datafile data.txt --covarfile covariates.txt --phenofile phenotypes.txt --gsave
-jwakim$ python glint.py --datafile data.glint --ewas --pheno treatment --covar --minstd 0.02
+python glint.py --datafile data.glint --ewas --pheno treatment --covar --minstd 0.02
 python glint.py --plot --manhattan --results results.glint.linreg.txt
